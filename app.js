62aa047be6c6384d8bf4f1e10cebf32d12ff8cf1
Mladen <kovacevic_mladen@hotmail.com>
2022-10-12 14:32:40 +0200

    changed font color

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const makeRandomColor = () => {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    if (r + g + b <= 300) {
+    if (r + g + b <= 320) {
         document.body.style.color = "yellow"
     } else document.body.style.color = "black"
     return `rgb(${r}, ${g}, ${b})`
